ac4b6644da64d1803193aac52f6f1eb213b119a8
Ikl0 <klimovs2008van@gmail.com>
2023-02-28 11:53:42 +0300

    refactor task4

--- ikl0/hometask-1/task4.rb
+++ ikl0/hometask-1/task4.rb
@@ -1,13 +1,11 @@
+# frozen_string_literal: true
+
 max = -1
 999.downto(100) do |i|
   999.downto(100) do |x|
-    num = i*x
-    if num.to_s == num.to_s.reverse
-      pal=num
-      if pal.to_i > max
-        max = pal
-      end
-    end
+    num = i * x
+    pal = num if num.to_s == num.to_s.reverse
+    max = pal if pal.to_i > max
   end
 end
 puts max
