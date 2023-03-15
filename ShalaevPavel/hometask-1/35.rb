40df54deb6057ee32a20853061427e1cb4709a4d
Pavel Shalaev <93087256+ShalaevPavel@users.noreply.github.com>
2023-03-15 11:30:40 +0300

    Update 35.rb
    Cleaning

--- ShalaevPavel/hometask-1/35.rb
+++ ShalaevPavel/hometask-1/35.rb
@@ -1,12 +1,5 @@
 require 'prime'
 
-def all_rotated(number_str)
-  arr = number_str.split('')
-  p arr.rotate(2)
-end
-# all_rotated("1536")
-# p [1, 2, 4, 6].rotate(3)
-
 def is_circular?(number)
   converted = number.to_s.split('')
   (0..converted.length - 1).each do |i|
@@ -18,4 +11,4 @@ def is_circular?(number)
 end
 
 # p (0..100).reduce{|x, counter| counter + is_circular?(x).to_i }
-p (1..2_000_000).select {|x| is_circular?(x)}.count
+p (1..1_000_000).select {|x| is_circular?(x)}.count
