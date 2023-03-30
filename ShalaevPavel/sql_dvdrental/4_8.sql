97527bc5807df2708ccc98a745f08b3e1683b188
ShalaevPavel <lohgewt123@yandex.by>
2023-03-30 16:55:11 +0300

    8 works

--- ShalaevPavel/sql_dvdrental/4_8.sql
+++ ShalaevPavel/sql_dvdrental/4_8.sql
@@ -1,2 +1,35 @@
-DELETE FROM film
-WHERE film_id = 3 CASCADE;
+ALTER TABLE film_actor
+DROP
+CONSTRAINT film_actor_film_id_fkey
+     , ADD  CONSTRAINT film_actor_film_id_fkey
+   FOREIGN KEY (film_id) REFERENCES film (film_id) ON DELETE
+CASCADE;
+
+
+ALTER TABLE film_category
+DROP
+CONSTRAINT film_category_film_id_fkey
+     , ADD  CONSTRAINT film_category_film_id_fkey
+   FOREIGN KEY (film_id) REFERENCES film (film_id) ON DELETE
+CASCADE;
+
+
+ALTER TABLE inventory
+DROP
+CONSTRAINT inventory_film_id_fkey
+     , ADD  CONSTRAINT inventory_film_id_fkey
+   FOREIGN KEY (film_id) REFERENCES film (film_id) ON DELETE
+CASCADE;
+
+
+ALTER TABLE rental
+DROP
+CONSTRAINT rental_inventory_id_fkey
+     , ADD  CONSTRAINT rental_inventory_id_fkey
+   FOREIGN KEY (film_id) REFERENCES film (film_id) ON DELETE
+CASCADE;
+
+
+DELETE
+FROM film
+WHERE film_id = 14;
